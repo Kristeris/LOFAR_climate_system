fd1acd217809798f4f552442253cb34e1a7e038c
Karsonics <karsonics@gmail.com>
2026-01-16 10:37:29 +0200

    trying to make a data filter

---
+++ LOFAR-system/angular/src/app/components/data-filter/data-filter.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-filter',
+  imports: [],
+  templateUrl: './data-filter.html',
+  styleUrl: './data-filter.css',
+})
+export class DataFilter {
+
+}
